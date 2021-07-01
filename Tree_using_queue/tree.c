3b748075353bb102e0be49664525c54658c5709a
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-07-01 23:25:45 +0300

    Update tree.c

--- Tree_using_queue/tree.c
+++ Tree_using_queue/tree.c
@@ -1,54 +1,82 @@
 #include "stdio.h"
 #include "queue.h"
+#include "stdlib.h"
 
 Node *root=NULL;
 
 void create(void);
 void DisplayInOrder(Node *);
+void FreeTree(Node *);
 
 void create(void)
 {
     Node *p, *t;
-    int x, cnt=0;
+    int x, space=10, i=0, cnt_nodes=2;
 
     puts("### TO EXIT ENTER -1 ###");
-    puts("Enter root value ");
+    printf("Enter root value \n");
     scanf("%d", &x);
     root = Init_node();
     root->data = x;
-    root->prec = 0;
+    root->space = 0;
     enqueue(root);
+    Q->cnt_nodes = 1;
 
     while (!IsEmptyQ(Q)) {
         p = dequeue();
-        printf("Enter left child of %d ", p->data);
+        printf("Enter left child of %d \n", p->data);
         scanf("%d", &x);
         if (x != -1) {
             t = Init_node();
             t->data = x;
-            t->prec = cnt+1;
+            t->space = space;
             p->lchild = t;
             enqueue(t);
+            i++;
         }
-        printf("Enter right child of %d ", p->data);
-        scanf("%d.4", &x);
+        printf("Enter right child of %d \n", p->data);
+        scanf("%d", &x);
         if (x != -1) {
             t = Init_node();
             t->data = x;
-            t->prec = cnt+1;
+            t->space = space;
             p->rchild = t;
             enqueue(t);
+            i++;
         }
-        cnt++;
+        if (i == cnt_nodes) {
+            printf("[!] Tree is balanced\n");
+            cnt_nodes *= 2;
+            i = 0;
+            space += 5;
+        }
+        Q->cnt_nodes++;
     }
+    system("cls");
+    printf("\n\n");
 }
 
 void DisplayInOrder(Node *r)
 {
+    int i;
     if (r) {
-        printf("%d\n", r->data);
         DisplayInOrder(r->lchild);
+        for (i=0; i < r->space; i++)
+            printf(" ");
+        if (r->space == 0)
+            printf("{ROOT} [%d]\n", r->data);
+        else
+            printf(" ----[%d\n", r->data);
         DisplayInOrder(r->rchild);
+    }
+}
+
+void FreeTree(Node *r)
+{
+    if (r) {
+        FreeTree(r->lchild);
+        FreeTree(r->rchild);
+        free((Node *) r);
     }
 }
 
@@ -56,6 +84,10 @@ void DisplayInOrder(Node *r)
 int main(void) {
 
     create();
+    puts("[+] Printing Tree ...");
+    printf("[+] %d Nodes\n", Q->cnt_nodes-1);
     DisplayInOrder(root);
+    FreeTree(root);
+    puts("[*] Freeing Tree ...");
     return 0;
 }
