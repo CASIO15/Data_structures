a516c5c2eed0318e3a1fc93250eea643788935a9
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-10-24 23:19:09 +0300

    Create queue.h

---
+++ Binary_tree/headers/queue.h
@@ -0,0 +1,24 @@
+#ifndef GEEKSFORGEEKS_QUEUE_H
+#define GEEKSFORGEEKS_QUEUE_H
+
+#include "tree.h"
+
+
+typedef struct l_node {
+    t_node *node;
+    struct l_node *next;
+} list_node;
+
+typedef struct queue {
+    list_node *front;
+    list_node *rear;
+} Queue;
+
+#define IsEmpty(q) ((q)->rear == NULL && (q)->front == NULL) ? 1 : 0
+
+
+Queue *InitQueue();
+void EnQueue(Queue **queue, t_node *node);
+t_node *DeQueue(Queue **queue);
+
+#endif
