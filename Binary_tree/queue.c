5091bc8304697faad8bf904d9226ca01ec7ff03b
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-10-24 23:19:24 +0300

    Create queue.c

---
+++ Binary_tree/queue.c
@@ -0,0 +1,50 @@
+#include "queue.h"
+
+Queue *InitQueue()
+{
+    Queue *new;
+
+    if ((new = malloc(sizeof(Queue))) == NULL) {
+        return NULL;
+    }
+    new->front = new->rear = NULL;
+
+    return new;
+}
+
+void EnQueue(Queue **queue, t_node *node)
+{
+    list_node *new_node;
+
+    if ((new_node = malloc(sizeof(list_node))) == NULL) {
+        return;
+    }
+
+    new_node->node = node;
+    new_node->next = NULL;
+
+    if (*queue == NULL) {
+        if ((*queue = InitQueue()) == NULL)
+            return;
+        (*queue)->front = (*queue)->rear = new_node;
+        (*queue)->rear->next = NULL;
+        return;
+    }
+
+    (*queue)->rear->next = new_node;
+    (*queue)->rear = new_node;
+}
+
+t_node *DeQueue(Queue **queue)
+{
+    if (IsEmpty(*queue) == 1)
+        return NULL;
+
+    list_node *current = (*queue)->front;
+    t_node *return_val = current->node;
+
+    (*queue)->front = (*queue)->front->next;
+    free(current);
+
+    return return_val;
+}
