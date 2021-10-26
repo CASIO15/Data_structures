78f6c121ae009ccfc3e806c0b6529c46dea475c9
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-10-27 00:24:36 +0300

    Update tree.h

--- Binary_tree/headers/tree.h
+++ Binary_tree/headers/tree.h
@@ -1,18 +1,21 @@
-#ifndef GEEKSFORGEEKS_TREE_H
-#define GEEKSFORGEEKS_TREE_H
+#ifndef TREE_H
+#define TREE_H
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
 
 typedef struct node {
-    struct node *lchild;
     int data;
+    struct node *lchild;
     struct node *rchild;
 } t_node;
 
 typedef t_node Tree;
 
+#define IS_LEAF(root) ((root)->lchild == NULL && (root)->rchild == NULL) ? 1 : 0
+#define IS_R_LEAF(root) ((root)->rchild == NULL) ? 1 : 0
+#define IS_L_LEAF(root) ((root)->lchild == NULL) ? 1 : 0
 
 t_node *init_node(int data);
 void InOrder(Tree *root);
@@ -24,5 +27,8 @@ int SizeOfTree(Tree *root);
 int FindMax(Tree *root);
 int FindMin(Tree *root);
 void PrintKLevelIterative(Tree *root);
+void insert(Tree *root, int key);
+t_node *FindDeepestNode(Tree *root);
+t_node *delete(Tree *root, t_node *head_ref, t_node *deepest, int key);
 
 #endif
