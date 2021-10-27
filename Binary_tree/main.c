3887b51c6c96d050e342369a19227a864d2ed431
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-10-27 22:29:13 +0300

    Update main.c

--- Binary_tree/main.c
+++ Binary_tree/main.c
@@ -1,35 +1,38 @@
 #include <stdio.h>
 #include "tree.h"
 
-
 int main()
 {
-    Tree *root = init_node(4);
+    Tree root = *init_node(-20);
 
-    root->lchild = init_node(5);
+    root.lchild = init_node(-19);
+    root.rchild = init_node(-1);
 
-    root->rchild = init_node(2);
-    root->rchild->lchild = init_node(3);
-    root->rchild->rchild = init_node(1);
-    root->rchild->lchild->lchild = init_node(6);
-    root->rchild->lchild->rchild = init_node(7);
+   root.rchild->lchild = init_node(-1);
+   root.rchild->rchild = init_node(0);
 
 
 
+    if (isCSum(&root) != INT_MIN)
+        printf("True\n");
+    else
+        printf("False\n");
+
+/*
     printf("Left view: ");
-    LeftView(root, 0);
+    LeftView(&root, 0);
     puts("");
 
     printf("Left view iterative: ");
-    LeftViewIterative(root);
+    LeftViewIterative(&root);
 
 
     printf("\nLevel order traversal: ");
-    PrintKLevelIterative(root);
-    printf("The size of the tree is: %d\n", SizeOfTree(root));
-    printf("The max element is: %d\n", FindMax(root));
-    printf("The min element is: %d\n", FindMin(root));
-
+    PrintKLevelIterative(&root);
+    printf("The size of the tree is: %d\n", SizeOfTree(&root));
+    printf("The max element is: %d\n", FindMax(&root));
+    printf("The min element is: %d\n", FindMin(&root));
+    */
 
     return 0;
 }
