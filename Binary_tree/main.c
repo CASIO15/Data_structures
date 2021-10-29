89a65feb5835f5812fa129e1fed01f3172eebab0
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-10-29 11:56:13 +0300

    Update main.c

--- Binary_tree/main.c
+++ Binary_tree/main.c
@@ -3,37 +3,39 @@
 
 int main()
 {
+    Tree *root = init_node(10);
 
-    Tree *root = init_node(1000);
+    root->lchild = init_node(8);
+    root->lchild->lchild = init_node(4);
+    root->lchild->rchild = init_node(4);
+    root->lchild->rchild->lchild = init_node(4);
 
-    root->lchild = init_node(590);
-    root->rchild = init_node(410);
+    root->rchild = init_node(2);
 
-    root->lchild->lchild = init_node(500);
-    root->lchild->rchild = init_node(90);
+    if (CheckHeightBalance(root))
+        printf("Tree is balanced !\n");
+    else
+        printf("The tree is not balanced !\n");
 
-    root->rchild->rchild = init_node(410);
-
-
-    if (isCSum(root) == root->data)
+    if (isCSum(root) == 1)
         printf("True\n");
     else
         printf("False\n");
 
 
     printf("Left view: ");
-    LeftView(&root, 0);
+    LeftView(root, 0);
     puts("");
 
     printf("Left view iterative: ");
-    LeftViewIterative(&root);
+    LeftViewIterative(root);
 
 
     printf("\nLevel order traversal: ");
-    PrintKLevelIterative(&root);
-    printf("The size of the tree is: %d\n", SizeOfTree(&root));
-    printf("The max element is: %d\n", FindMax(&root));
-    printf("The min element is: %d\n", FindMin(&root));
+    PrintKLevelIterative(root);
+    printf("The size of the tree is: %d\n", SizeOfTree(root));
+    printf("The max element is: %d\n", FindMax(root));
+    printf("The min element is: %d\n", FindMin(root));
 
     return 0;
 }
