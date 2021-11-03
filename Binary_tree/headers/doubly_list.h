f0a07e62d1459a4f4b8a22ef8e57d54c31128617
Tomer <71800501+CASIO15@users.noreply.github.com>
2021-11-03 21:37:28 +0200

    Create doubly_list.h

---
+++ Binary_tree/headers/doubly_list.h
@@ -0,0 +1,16 @@
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
+#include "tree.h"
+
+typedef struct double_list {
+    t_node *node;
+    struct double_list *flink;
+    struct double_list *blink;
+} DList;
+
+DList *init_list();
+void insert_list(DList **list, t_node *node);
+void free_list(DList **list);
+
+#endif
